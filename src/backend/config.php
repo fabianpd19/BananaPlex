ff8c038af291b884e28047459965ccc06799fd09
fabianpd19 <fapalma1@espe.edu.ec>
2024-06-20 22:10:26 -0500

    Proyecto dockerizado

--- src/backend/config.php
+++ src/backend/config.php
@@ -1,11 +1,8 @@
 <?php
-// $host = '192.168.1.18'; // Cambia esto por la dirección de tu servidor PostgreSQL
-// $host = '10.241.0.67'; // Cambia esto por la dirección de tu servidor PostgreSQL
-// $host = '10.241.0.56'; // Cambia esto por la dirección de tu servidor PostgreSQL
-$host = '192.168.0.7'; // Cambia esto por la dirección de tu servidor PostgreSQL
-$dbname = 'BananaPlex2'; // Nombre de tu base de datos
-$user = 'Grupo3'; // Usuario de la base de datos
-$password = 'gestiongrupo3'; // Contraseña del usuario
+$host = 'db';
+$dbname = 'BananaPlex';
+$user = 'Grupo3';
+$password = 'gestiongrupo3';
 
 try {
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
