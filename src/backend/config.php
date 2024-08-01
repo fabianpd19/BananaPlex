66d3175f6cb5d948891e16cfe0e34d3d298f4dec
fabianpd19 <fapalma1@espe.edu.ec>
2024-07-31 23:20:22 -0500

    Arreglar solicitudes

--- src/backend/config.php
+++ src/backend/config.php
@@ -4,10 +4,10 @@
 // $user = 'Grupo3';
 // $password = 'gestiongrupo3';
 
-$host = 'localhost';
-$dbname = 'prueba';
-$user = 'postgres';
-$password = 'espeF19kplm19';
+$host = 'db';
+$dbname = 'BananaPlex';
+$user = 'Grupo3';
+$password = 'gestiongrupo3';
 
 try {
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
